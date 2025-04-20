5ef0bec1c53d9c48efe6335aed3457194c108516
Antonio Tsai <antonio20051110@gmail.com>
2025-04-21 00:03:57 +0800

    changed data headers into one liner

--- config.py
+++ config.py
@@ -1,14 +1,5 @@
-
-dataFileName = "test"
 Number_of_Players = 3
 Number_of_Decks = 3
 sim_rounds = 100
-
-def field():
-    temp = ['Rounds','Dealer']
-    for i in range(1,Number_of_Players+1):
-        temp.append(f'Player_{str(i)}')
-    temp.append('Card_Code')
-    return temp
-
-fileheaders = field()
+fileheaders = ['Rounds','Dealer'] + [f'Player_{str(i)}' for i in range(1,Number_of_Players+1)] + ['Card_Code']
+dataFileName = "Sim_rd_" + str(sim_rounds) + "_NoP_" + str(Number_of_Players) + "_NoD_" + str(Number_of_Decks)
